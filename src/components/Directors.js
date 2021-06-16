9fb8c63a6a45bb28bb248c84d23f6f002d163961
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-06-16 00:41:45 -0500

    commit tree 239e3ea543825d2635f951436bd1c0bef3df4db8

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const directorMapper = () => {
+    return directors.map(director => {
+      return <div> {director.name}
+        <ul> {director.movies.map(movie => {
+          return <li key={movie}>{movie}</li> })} 
+        </ul>
+      </div>
+  })}
+
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Directors Page</h1>
+        {directorMapper()}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
