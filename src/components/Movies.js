96508615c5c5ac3570fd630b04505068b26612e6
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-06-16 00:41:55 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const movieMapper = () => {
+    return movies.map(movie => {
+      return <div> {movie.title}: {movie.time}
+        <ul> {movie.genres.map(genre => {
+          return <li key={genre}>{genre}</li> })} 
+        </ul>
+      </div>
+  })}
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movieMapper()}
     </div>
   );
 };
